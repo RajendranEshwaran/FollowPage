314b07330dbbc7866e68f73d7b053eed342bc9a5
RAJENDRAN ESHWARAN <10699484+RajendranEshwaran@users.noreply.github.com>
2025-07-21 14:20:04 -0400

    Initial Commit

---
+++ FollowPage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FollowPage
+//
+//  Created by RajayGoms on 7/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FollowPage/FollowPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  FollowPageApp.swift
+//  FollowPage
+//
+//  Created by RajayGoms on 7/21/25.
+//
+
+import SwiftUI
+
+@main
+struct FollowPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
